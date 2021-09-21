4faaabac5dc1fe7106e6582a4920cbdc55a292fd
Enricco Diniz Leite do Amaral <enriccoamaral@hotmail.com>
2021-09-21 13:15:09 -0300

    Desafio 2!

---
+++ MarvelDotNet/MarvelDotNet/Migrations/20210919045722_InitClients.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MarvelDotNet.Migrations
+{
+    public partial class InitClients : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Clients",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    Age = table.Column<int>(type: "INTEGER", nullable: false),
+                    CPF = table.Column<string>(type: "TEXT", nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Phone = table.Column<string>(type: "TEXT", nullable: false),
+                    Address = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Clients", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Clients");
+        }
+    }
+}
---
+++ MarvelDotNet/MarvelDotNet/Models/Client.cs
@@ -0,0 +1,51 @@
+﻿using MarvelDotNet.Utils.Validations;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace MarvelDotNet.Models
+{
+    // nome, idade, CPF, e-mail, telefone e endereço.
+    public class Client
+    {
+        public Guid Id {  get; set; }
+
+        [Required]
+        public String Name { get;  set; }
+
+        [Required]
+        [Range(0, Int32.MaxValue)]
+        public Int32 Age { get;  set; }
+
+        [Required]
+        [CPFValidator]
+        public String CPF { get;  set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "Not Valid Format")]
+        public String Email { get;  set; }
+
+        [Required]
+        [Phone]
+        public String Phone { get;  set; }
+
+        [Required]
+        public String Address { get;  set; }
+
+        public Client(string name, int age, string cPF, string email, string phone, string address)
+        {
+            Name = name;
+            Age = age;
+            CPF = cPF;
+            Email = email;
+            Phone = phone;
+            Address = address;
+        }
+
+        public override string ToString()
+        {
+            return $"ID={this.Id}, {this.Name}, {this.Email}, {this.Phone}, {this.CPF}, {this.Age}";
+        }
+    }
+}
